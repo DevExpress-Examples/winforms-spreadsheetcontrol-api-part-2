f2dd888107efd478aed315a51c7bd0eeeed5cc7c
DevExpressExampleBot <scmaintenance@devexpress.com>
2026-09-23 13:01:08 +0400

    Source auto update [skip ci]

--- VB/SpreadsheetControl_API_Part02/SpreadsheetActions/TableActions.vb
+++ VB/SpreadsheetControl_API_Part02/SpreadsheetActions/TableActions.vb
@@ -1,33 +1,26 @@
-﻿Imports DevExpress.Spreadsheet
+Imports DevExpress.Spreadsheet
 Imports System
-Imports System.Collections.Generic
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 
 Namespace SpreadsheetControl_API
-    Public NotInheritable Class TableActions
-#Region "Actions"
-        Public Shared CreateTableAction As Action(Of IWorkbook) = AddressOf CreateTable
-        Public Shared CustomTableStyleAction As Action(Of IWorkbook) = AddressOf CustomTableStyle
-#End Region
 
+    Public Module TableActions
 
-        Private Sub New()
-        End Sub
-        Private Shared Sub CreateTable(ByVal workbook As IWorkbook)
+'#Region "Actions"
+        Public CreateTableAction As Action(Of IWorkbook) = AddressOf CreateTable
+
+        Public CustomTableStyleAction As Action(Of IWorkbook) = AddressOf CustomTableStyle
+
+'#End Region
+        Private Sub CreateTable(ByVal workbook As IWorkbook)
             workbook.BeginUpdate()
-
             Dim worksheet As Worksheet = workbook.Worksheets(0)
             GenerateTableData(worksheet)
-            '#Region "#CreateTable"
+'#Region "#CreateTable"
             ' Insert a table in the worksheet.
             Dim table As Table = worksheet.Tables.Add(worksheet("B2:F5"), True)
-
             ' Format the table by applying a built-in table style.
             table.Style = workbook.TableStyles(BuiltInTableStyleId.TableStyleMedium27)
-
             ' Access table columns and name them.
             Dim productColumn As TableColumn = table.Columns(0)
             productColumn.Name = "Product"
@@ -39,89 +32,79 @@ Namespace SpreadsheetControl_API
             discountColumn.Name = "Discount"
             Dim amountColumn As TableColumn = table.Columns(4)
             amountColumn.Name = "Amount"
-
             ' Set the formula to calculate the amount per product 
             ' and display results in the "Amount" column.
             amountColumn.Formula = "=[Price]*[Quantity]*(1-[Discount])"
-
             ' Display the total row in the table.
             table.ShowTotals = True
-
             ' Set the label and function to display the sum of the "Amount" column.
             discountColumn.TotalRowLabel = "Total:"
             amountColumn.TotalRowFunction = TotalRowFunction.Sum
-
             ' Specify the number format for each column.
             priceColumn.DataRange.NumberFormat = "$#,##0.00"
             discountColumn.DataRange.NumberFormat = "0.0%"
             amountColumn.Range.NumberFormat = "$#,##0.00;$#,##0.00;"""";@"
-
             ' Specify horizontal alignment for header and total rows of the table.
             table.HeaderRowRange.Alignment.Horizontal = SpreadsheetHorizontalAlignment.Center
             table.TotalRowRange.Alignment.Horizontal = SpreadsheetHorizontalAlignment.Center
-
             ' Specify horizontal alignment to display data in all columns except the first one.
             For i As Integer = 1 To table.Columns.Count - 1
                 table.Columns(i).DataRange.Alignment.Horizontal = SpreadsheetHorizontalAlignment.Center
-            Next i
+            Next
 
             ' Set the width of table columns.
             table.Range.ColumnWidthInCharacters = 10
-            '#End Region ' #CreateTable
+'#End Region  ' #CreateTable
             workbook.EndUpdate()
         End Sub
 
-        Private Shared Sub CustomTableStyle(ByVal workbook As IWorkbook)
+        Private Sub CustomTableStyle(ByVal workbook As IWorkbook)
             CreateTable(workbook)
             workbook.BeginUpdate()
             Dim worksheet As Worksheet = workbook.Worksheets(0)
-            '#Region "#CustomTableStyle"
+'#Region "#CustomTableStyle"
             ' Access a table.
             Dim table As Table = worksheet.Tables(0)
-
             Dim styleName As String = "testTableStyle"
-
             ' If the style under the specified name already exists in the collection,
             If workbook.TableStyles.Contains(styleName) Then
                 ' apply this style to the table.
                 table.Style = workbook.TableStyles(styleName)
             Else
                 ' Add a new table style under the "testTableStyle" name to the TableStyles collection.
-                Dim customTableStyle As TableStyle = workbook.TableStyles.Add("testTableStyle")
-
+                Dim lCustomTableStyle As TableStyle = workbook.TableStyles.Add("testTableStyle")
                 ' Modify the required formatting characteristics of the table style. 
                 ' Specify the format for different table elements.
-                customTableStyle.BeginUpdate()
+                lCustomTableStyle.BeginUpdate()
                 Try
-                    customTableStyle.TableStyleElements(TableStyleElementType.WholeTable).Font.Color = Color.FromArgb(107, 107, 107)
-
+                    lCustomTableStyle.TableStyleElements(TableStyleElementType.WholeTable).Font.Color = Color.FromArgb(107, 107, 107)
                     ' Specify formatting characteristics for the table header row. 
-                    Dim headerRowStyle As TableStyleElement = customTableStyle.TableStyleElements(TableStyleElementType.HeaderRow)
+                    Dim headerRowStyle As TableStyleElement = lCustomTableStyle.TableStyleElements(TableStyleElementType.HeaderRow)
                     headerRowStyle.Fill.BackgroundColor = Color.FromArgb(64, 66, 166)
                     headerRowStyle.Font.Color = Color.White
                     headerRowStyle.Font.Bold = True
-
                     ' Specify formatting characteristics for the table total row. 
-                    Dim totalRowStyle As TableStyleElement = customTableStyle.TableStyleElements(TableStyleElementType.TotalRow)
+                    Dim totalRowStyle As TableStyleElement = lCustomTableStyle.TableStyleElements(TableStyleElementType.TotalRow)
                     totalRowStyle.Fill.BackgroundColor = Color.FromArgb(115, 193, 211)
                     totalRowStyle.Font.Color = Color.White
                     totalRowStyle.Font.Bold = True
-
                     ' Specify banded row formatting for the table.
-                    Dim secondRowStripeStyle As TableStyleElement = customTableStyle.TableStyleElements(TableStyleElementType.SecondRowStripe)
+                    Dim secondRowStripeStyle As TableStyleElement = lCustomTableStyle.TableStyleElements(TableStyleElementType.SecondRowStripe)
                     secondRowStripeStyle.Fill.BackgroundColor = Color.FromArgb(234, 234, 234)
                     secondRowStripeStyle.StripeSize = 1
                 Finally
-                    customTableStyle.EndUpdate()
+                    lCustomTableStyle.EndUpdate()
                 End Try
+
                 ' Apply the created custom style to the table.
-                table.Style = customTableStyle
+                table.Style = lCustomTableStyle
             End If
-            '#End Region ' #CustomTableStyle
+
+'#End Region  ' #CustomTableStyle
             workbook.EndUpdate()
         End Sub
 
-        Public Shared Sub GenerateTableData(ByVal sheet As Worksheet)
+        Public Sub GenerateTableData(ByVal sheet As Worksheet)
             sheet.Cells("B3").SetValue("Chocolade")
             sheet.Cells("B4").SetValue("Konbu")
             sheet.Cells("B5").SetValue("Geitost")
@@ -135,5 +118,5 @@ Namespace SpreadsheetControl_API
             sheet.Cells("E4").SetValue(0.1)
             sheet.Cells("E5").SetValue(0.07)
         End Sub
-    End Class
+    End Module
 End Namespace
